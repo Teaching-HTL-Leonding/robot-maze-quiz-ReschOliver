8e20db1f3514d1cfc2ce15b882360fe84b154b1a
OliverResch <oliver.resch@outlook.at>
2020-10-07 22:56:09 +0200

    edited MoveRobotToExit()

--- Maze.Robot/RobotController.cs
+++ Maze.Robot/RobotController.cs
@@ -1,4 +1,5 @@
 ﻿using Maze.Library;
+using System;
 
 namespace Maze.Solver
 {
@@ -33,13 +34,21 @@ namespace Maze.Solver
         {
             // Here you have to add your code
 
-            // Trivial sample algorithm that can just move right
             var reachedEnd = false;
             robot.ReachedExit += (_, __) => reachedEnd = true;
-
+            Random rnd = new Random();
+            int moves = 0;
             while (!reachedEnd)
             {
-                robot.Move(Direction.Right);
+                robot.TryMove((Direction)rnd.Next(0, 4));
+
+                moves++;
+                if (moves == 10000000)
+                {
+                    robot.HaltAndCatchFire();
+                    break;
+                }
+
             }
         }
     }
